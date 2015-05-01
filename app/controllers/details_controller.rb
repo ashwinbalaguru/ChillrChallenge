0f66143da80a1a56492c41b9d7d05590be07b7e4
ashwin <ashwinbalaguru.c@gmail.com>
2015-05-01 18:20:49 +0530

    City with highest transaction

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -2,6 +2,7 @@ class DetailsController < ApplicationController
   def index
   	@details = Detail.where("status = 'success'")
   	@total_success_trans = total_success_trans
+  	@highest_value_trans = highest_value_trans
   end
 
   def total_success_trans
@@ -9,4 +10,14 @@ class DetailsController < ApplicationController
     total_trans = Detail.all.count
     total_percent_success_trans = (no_successful_trans.to_f / total_trans)*100
   end
+
+  	def highest_value_trans
+		@city = Detail.all.collect {|p| p.city}.uniq
+		@city_count = Detail.all.collect {|p| p.city}.uniq.count
+		@select_city = []
+		@city.each do |city|
+			scity = Detail.where(["city = ? and status = ?", city, "success"])
+			@select_city +=scity if scity
+		end
+	end
 end
