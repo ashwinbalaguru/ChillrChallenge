37f53c1d1c5ab9cad4714528c6c80880f87869fa
ashwin <ashwinbalaguru.c@gmail.com>
2015-05-01 20:06:07 +0530

    Value and no of transaction

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,8 +1,9 @@
 class DetailsController < ApplicationController
   def index
-  	@details = Detail.where("status = 'success'")
+  	@details = Detail.all
   	@total_success_trans = total_success_trans
   	@highest_value_trans = highest_value_trans
+    @highest_no_trans = highest_no_trans
   end
 
   def total_success_trans
@@ -11,13 +12,23 @@ class DetailsController < ApplicationController
     total_percent_success_trans = (no_successful_trans.to_f / total_trans)*100
   end
 
-  	def highest_value_trans
+  def highest_value_trans
 		@city = Detail.all.collect {|p| p.city}.uniq
-		@city_count = Detail.all.collect {|p| p.city}.uniq.count
+		# @city_count = Detail.all.collect {|p| p.city}.uniq.count
 		@select_city = []
 		@city.each do |city|
 			scity = Detail.where(["city = ? and status = ?", city, "success"])
 			@select_city +=scity if scity
 		end
 	end
+
+  def highest_no_trans
+    @allcity = Detail.all.collect {|p| p.city}.uniq
+    @select_no_city = []
+    @allcity.each do |no_city|
+      scity = Detail.where(["city = ?", no_city])
+      @select_no_city +=scity if scity
+    end
+  end
+
 end
