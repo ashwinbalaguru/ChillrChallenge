83f97c0ffc80b79fc01118947f8380f61e447268
ashwin <ashwinbalaguru.c@gmail.com>
2015-05-01 14:47:11 +0530

    Total percentage of successful transaction

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,5 +1,12 @@
 class DetailsController < ApplicationController
   def index
-  	@details = Detail.all
+  	@details = Detail.where("status = 'success'")
+  	@total_success_trans = total_success_trans
+  end
+
+  def total_success_trans
+    no_successful_trans = Detail.where("status = 'success'").count
+    total_trans = Detail.all.count
+    total_percent_success_trans = (no_successful_trans.to_f / total_trans)*100
   end
 end
